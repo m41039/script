7c8bf97f08e7f953f2738474ebbddea42f5d6602
Mickey <m41039@yahoo.com.tw>
2017-03-03 08:54:40 +0800

    Create vipmx.sh
    his script will {start|stop|restart} vippmx sql service when DB deadlock

---
+++ vipmx.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+#This is Mickey's second script
+#This script will {start|stop|restart} vippmx sql service when DB deadlock
+#2017/02/22  Frist release
+
+now="$(date +'%Y%m%d')"
+hour=`date +'%H'`
+if [ $hour -le 12 ];then
+        shit=00
+else
+        shit=12
+fi
+######Stop A10
+case $1 in
+stop) 
+
+sudo /script/_mic/maintain/sigt_deny.sh;sleep 3;tail -f /opt/httpd/logs/access_vip_io_log$now$shit
+
+;;
+
+######Restart Sql
+restart)
+
+/script/webuse/wlc/vip_startwlc.sh
+;;
+
+#######Start A10 and check log
+start)
+
+sudo /script/_mic/maintain/sigt_allow.sh;sleep 3;tail -f /opt/httpd/logs/access_vip_io_log$now$shit
+;;
+
+*)
+echo $"Usage: {start|stop|restart}"
+        exit 1
+        ;;
+esac
