141d995a0227c07b284b09fc5cf120321b9ffe88
Mickey <m41039@yahoo.com.tw>
2017-03-15 18:37:30 +0800

    update sed function

--- adduser.sh
+++ adduser.sh
@@ -7,11 +7,12 @@ USER=gary
 PASSWD=gary978
 
 # Adduser gary and set passwd
-#PATH=/usr/lib/qt-3.3/bin:/usr/kerberos/sbin:/usr/kerberos/bin:/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin:/root/bin
-#export $PATH
-
 adduser $USER
 echo $PASSWD | passwd "$USER" --stdin
 
 # Add Gary to sudoers
 echo 'gary  ALL=(ALL:ALL) ALL' >> /etc/sudoers
+
+# Set PATH to user gary
+sed -i "s|PATH=.*|PATH=/usr/lib/qt-3.3/bin:/usr/kerberos/sbin:/usr/kerberos/bin:/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin:/root/bin/|g"  .bashrc
+source ~/.bashrc
