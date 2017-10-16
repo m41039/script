d1227442b43345a82cb060c7aa73ce7288ffcf92
Mickey <m41039@yahoo.com.tw>
2017-10-16 09:43:04 +0800

    modify cp to rsync

--- bsap_backup.sh
+++ bsap_backup.sh
@@ -5,17 +5,16 @@
 
 date=`date +%Y%m%d`
 
-cp /opt/others/bsap_export/1111/"$date"_FFO_C.zip  /opt/others/bsap_upload/508/
-cp /opt/others/bsap_export/1111/"$date"_FFO_J.zip  /opt/others/bsap_upload/509/
-cp /opt/others/bsap_export/1111/"$date"_FFO_S.zip  /opt/others/bsap_upload/510/
-cp /opt/others/bsap_export/yes123/"$date"_YES_C.zip  /opt/others/bsap_upload/511/
-cp /opt/others/bsap_export/yes123/"$date"_YES_J.zip  /opt/others/bsap_upload/512/
-cp /opt/others/bsap_export/518/"$date"_518_CnJ.zip  /opt/others/bsap_upload/513/
-cp /opt/others/bsap_export/518/"$date"_518_R.zip  /opt/others/bsap_upload/514/
-cp /opt/others/bsap_export/518/"$date"_518_H.zip  /opt/others/bsap_upload/527/
-cp /opt/others/bsap_export/ejob/"$date"_EJOB_CnJ.zip  /opt/others/bsap_upload/515/
-cp /opt/others/bsap_export/ejob/"$date"_EJOB_R.zip  /opt/others/bsap_upload/516/
-cp /opt/others/bsap_export/okwork/"$date"_OKWork_CnJ.zip  /opt/others/bsap_upload/517/
-cp /opt/others/bsap_export/178/"$date"_178_CnJ.zip  /opt/others/bsap_upload/94820303538487442/
+rsync -arvh /opt/others/bsap_export/1111/*_FFO_C.zip  /opt/others/bsap_upload/508/
+rsync -arvh /opt/others/bsap_export/1111/*_FFO_J.zip  /opt/others/bsap_upload/509/
+rsync -arvh /opt/others/bsap_export/1111/*_FFO_S.zip  /opt/others/bsap_upload/510/
+rsync -arvh /opt/others/bsap_export/yes123/*_YES_C.zip  /opt/others/bsap_upload/511/
+rsync -arvh /opt/others/bsap_export/yes123/*_YES_J.zip  /opt/others/bsap_upload/512/
+rsync -arvh /opt/others/bsap_export/518/*_518_CnJ.zip  /opt/others/bsap_upload/513/
+rsync -arvh /opt/others/bsap_export/518/*_518_R.zip  /opt/others/bsap_upload/514/
+rsync -arvh /opt/others/bsap_export/twjob/*_TW_CnJ.zip  /opt/others/bsap_upload/515/
+rsync -arvh /opt/others/bsap_export/twjob/*_TW_R.zip  /opt/others/bsap_upload/516/
+rsync -arvh /opt/others/bsap_export/okwork/*_OKWork_CnJ.zip  /opt/others/bsap_upload/517/
+rsync -arvh /opt/others/bsap_export/178/*_178_CnJ.zip  /opt/others/bsap_upload/94820303538487442/
 
 echo `date +%y%m%d_%H:%M` >> /home/mickeywang/bsap_backup_log.txt
